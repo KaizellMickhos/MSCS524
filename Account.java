fc227fd4bdc38e1e56b2c10ff4b6035f7b3edf58
KaizellMickhos <kaizellmickhosadam@gmail.com>
2015-07-31 19:16:18 -0700

    Update Account.java

--- Account.java
+++ Account.java
@@ -1,1 +1,16 @@
-
+public class Account
+{
+  private double balance; //instance variable that stores the balance
+  
+  //constructor
+  public Account(double initialBalance)
+  {
+    //validate that initialBalance is greater than 0.0;
+    //if it is not, balance is initialized to the default value 0.0
+    
+    if (initialBalance > 0.0)
+    balance = initialBalance;
+  }//end Account constructor
+  
+  //credit (add) 
+}
