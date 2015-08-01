1179bbdba8b995d02abafdc379cd2bda08693e6d
KaizellMickhos <kaizellmickhosadam@gmail.com>
2015-07-31 19:21:52 -0700

    Update Account.java

--- Account.java
+++ Account.java
@@ -12,5 +12,15 @@ public class Account
     balance = initialBalance;
   }//end Account constructor
   
-  //credit (add) 
-}
+  //credit (add) an amount ti the account
+  public void credit(double amount)
+  {
+    balance = balance + amount;//add amount to balance
+  }//end method credit
+  
+  //return the account balance
+  public double getBalance()
+  {
+    return balance; //gives the value of balance to the calling method
+  }//end method getBalance
+}//end class Account
